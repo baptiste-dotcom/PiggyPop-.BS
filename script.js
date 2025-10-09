2a6b7fddddbc0124f6c39895ede1e4a3f7bf7b49
baptiste-dotcom <bscharroo66@gmail.com>
2025-10-09 18:57:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById('cloudIntro').classList.remove('hidden');
   });
 
-  document.getElementById('continueButton').addEventListener('click', function() {
-    const cloud = document.getElementById('cloudIntro');
-    cloud.style.transition = 'opacity 0.6s ease';
-    cloud.style.opacity = '0';
-    console.log("CONTINUER cliqué");
+document.getElementById('continueButton').addEventListener('click', function() {
+  const cloud = document.getElementById('cloudIntro');
+  cloud.style.transition = 'opacity 0.6s ease';
+  cloud.style.opacity = '0';
 
-    setTimeout(() => {
-      cloud.style.display = 'none';
-    }, 600);
-  });
+  setTimeout(() => {
+    cloud.parentNode.removeChild(cloud);
+    console.log("Nuage supprimé avec fondu");
+  }, 600);
+});
 
   document.getElementById("quit-btn").addEventListener("click", () => {
     document.getElementById("quit-dialog").classList.remove("hidden");
