ded0a86c8b9ffd63379b33192944b981c7cd0c8e
baptiste-dotcom <bscharroo66@gmail.com>
2025-10-09 19:10:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
+  // Bouton "Lance-toi dans l’épopée céleste"
   document.getElementById('play-btn').addEventListener('click', function(e) {
     e.preventDefault();
     document.getElementById('cloudIntro').classList.remove('hidden');
   });
 
-  document.getElementById('continueButton').addEventListener('click', function() {
-    const cloud = document.getElementById('cloudIntro');
-    cloud.classList.add('fade-out');
-    console.log("CONTINUER cliqué");
+  // Bouton "CONTINUER" disparition animée du nuage
+  ['click', 'touchstart'].forEach(evt => {
+    document.getElementById('continueButton').addEventListener(evt, function() {
+      const cloud = document.getElementById('cloudIntro');
+      cloud.classList.add('fade-out');
+      console.log("CONTINUER cliqué");
 
-    setTimeout(() => {
-      cloud.parentNode.removeChild(cloud);
-    }, 600); // durée du fondu
+      setTimeout(() => {
+        cloud.parentNode.removeChild(cloud);
+      }, 600); // Durée du fondu
+    });
   });
 
+  // Bouton "Quitter"
   document.getElementById("quit-btn").addEventListener("click", () => {
     document.getElementById("quit-dialog").classList.remove("hidden");
   });
 
+  // Bouton "Rester"
   document.getElementById("stay-btn").addEventListener("click", () => {
     document.getElementById("quit-dialog").classList.add("hidden");
   });
 
+  // Bouton "Sortie définitive"
   document.getElementById("exit-btn").addEventListener("click", () => {
     document.getElementById("quit-dialog").classList.add("hidden");
     document.getElementById("crying-pig").classList.remove("hidden");
